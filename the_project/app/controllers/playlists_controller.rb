9f8e2eb009acf692b70759a9fdd5d8c54ebfc159
sgalabov22 <sgalabov22@gmail.com>
2019-04-02 12:17:59 +0300

    Added shiny things

--- the_project/app/controllers/playlists_controller.rb
+++ the_project/app/controllers/playlists_controller.rb
@@ -19,6 +19,13 @@ class PlaylistsController < ApplicationController
     end
   end
 
+  def destroy
+    playlist = Playlist.find(params[:id])
+    playlist.destroy
+
+    redirect_to playlists_path
+  end
+
 private
   def playlist_params
     params.require(:playlist).permit(:name)
