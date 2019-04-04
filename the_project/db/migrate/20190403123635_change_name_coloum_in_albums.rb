eb23b70a9cb37ec95c066f6c59bc4a539c4c2d88
Chocho31 <chudomir.miroslavov.chenkov@gmail.com>
2019-04-04 19:37:32 +0300

    Minor update

--- the_project/db/migrate/20190403123635_change_name_coloum_in_albums.rb
+++
@@ -1,5 +0,0 @@
-class ChangeNameColoumInAlbums < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :albums, :name, :title
-  end
-end
--- the_project/db/migrate/20190403141900_change_title_albums.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTitleAlbums < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :albums, :title, :name
-  end
-end
