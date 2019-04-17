03d1d986eb33f38056141146b821c6cba623d6b6
sgalabov22 <sgalabov22@gmail.com>
2019-04-17 13:51:45 +0300

    init Heroku

--- the_project/db/schema.rb
+++ the_project/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_04_03_115538) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "albums", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
