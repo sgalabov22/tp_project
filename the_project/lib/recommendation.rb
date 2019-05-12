86dab3885fcd232ef09d08edbdfe9479cf7dff15
Chocho31 <chudomir.miroslavov.chenkov@gmail.com>
2019-05-12 22:49:09 +0300

    Added simple recommendation algorithm based on collaborative filtering, will work with liking songs and adding them to user songs field

---
+++ the_project/lib/recommendation.rb
@@ -0,0 +1,21 @@
+module Recommendation 
+
+    def recommended_songs
+        other_users = self.class.all.where.not('id != ?', self.id);
+
+        recommended = Hash.new(0)
+
+        other_users.each do |user|
+            common_songs = user.songs & self.songs
+
+            weight = common_songs.size.to_f / user.songs.size
+
+            (user.songs - common_songs).each do |song|
+                recommended[song] += weight
+            end
+        end
+
+        sorted_recommended = recommended.sort_by { |key, value| value }.reverse
+    end
+
+end
