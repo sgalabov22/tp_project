1e870312e8905af63a52965d0b153393f51e760d
sgalabov22 <sgalabov22@gmail.com>
2019-05-13 20:52:42 +0300

    Almost functioning play button in playlists

--- the_project/app/controllers/application_controller.rb
+++ the_project/app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
   before_action :authenticate_user!
-=======
-    before_action :authenticate_user!
->>>>>>> ca157544388b9d713b0ba9caef3230841dd6466c
 end
