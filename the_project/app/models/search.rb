4bf90d3b94bb823945ef25bc7d53ab7bb6008f55
Chocho31 <chudomir.miroslavov.chenkov@gmail.com>
2019-06-10 23:44:42 +0300

    In songs view now there is displayed table of recommended songs, and songs can be added

--- the_project/app/models/search.rb
+++ the_project/app/models/search.rb
@@ -1,6 +1,6 @@
 class Search < ApplicationRecord
     belongs_to :searchable, polymorphic: true
     scope :execute, -> (query) {
-        where('reverse_index like :query', query: "%#{query}%")
+        where('lower(reverse_index) like :query', query: "%#{query}%")
     }
 end
